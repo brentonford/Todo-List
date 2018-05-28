e766bf69389fd53f47566c1f6aa93794db5ea60d
BRENTON FORD <brenton.ford@synerty.com>
2018-05-29 05:23:57 +1000

    create new item

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,23 @@
 // Check off specific TODOs by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+// Click on delete todo
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// Create new todo
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
 });
