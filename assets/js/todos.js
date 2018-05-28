44a21ed5c7cb399ada82fdf0a45d5adbd0acf9da
BRENTON FORD <brenton.ford@synerty.com>
2018-05-29 05:05:37 +1000

    initial commit, add and remove items

---
+++ assets/js/todos.js
@@ -0,0 +1,11 @@
+// Check off specific TODOs by clicking
+$("li").click(function(){
+    $(this).toggleClass("completed");
+});
+
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
